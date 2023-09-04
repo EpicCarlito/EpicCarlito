612c09df5c7685921d5dc5e1964df8e4741b2fb1
EpicCarlito <jameschef2018@hotmail.com>
2023-09-04 08:36:35 -0400

    Pattern fixed?

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,7 +3,8 @@ import Pattern from "../assets/MainPattern.svg"
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4 bg-slate-50">
+    <div className="h-screen w-screen">
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
       <img src={Profile} height={100} width={100} className="rounded-full"></img>
       <div className="flex justify-center items-center gap-1">
         <h1 className="text-3xl font-comfortaa">
@@ -12,7 +13,8 @@ export default function Home() {
         <p className="flex flex-row-reverse text-sm">he/him</p>
       </div>
       <p>Hello, I am EpicCarlito (my online persona)!</p>
-      <img className="absolute bottom-0 z-[1] w-full bg-red-700" src={Pattern}></img>
+    </div>
+    <img className="absolute bottom-0 z-[-1] w-full" src={Pattern}></img>
     </div>
   )
 }
