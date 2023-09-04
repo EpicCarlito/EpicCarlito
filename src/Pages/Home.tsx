0061f5cb8dcdfdbcac8684f2d637fdc898593520
EpicCarlito <jameschef2018@hotmail.com>
2023-09-04 08:31:25 -0400

    Mobile Checks Fixed HTML

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,7 +3,7 @@ import Pattern from "../assets/MainPattern.svg"
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4">
+    <div className="flex flex-col items-center justify-center h-screen gap-4 bg-slate-50">
       <img src={Profile} height={100} width={100} className="rounded-full"></img>
       <div className="flex justify-center items-center gap-1">
         <h1 className="text-3xl font-comfortaa">
@@ -12,7 +12,7 @@ export default function Home() {
         <p className="flex flex-row-reverse text-sm">he/him</p>
       </div>
       <p>Hello, I am EpicCarlito (my online persona)!</p>
-      <img className="absolute bottom-0 z-[-1] w-full" src={Pattern}></img>
+      <img className="absolute bottom-0 z-[1] w-full bg-red-700" src={Pattern}></img>
     </div>
   )
 }
