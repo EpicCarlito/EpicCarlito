02f35222cb328c86c333b0d1d745eb825720d52a
EpicCarlito <jameschef2018@hotmail.com>
2023-12-01 15:18:52 -0500

    Fixed Mobile Issue on Error.tsx

--- src/Pages/Error.tsx
+++ src/Pages/Error.tsx
@@ -4,9 +4,9 @@ import Thinking from "../assets/ThinkingEmoji.svg"
 export default function Error() {
     return (
       <div className="h-full">
-        <div className="flex flex-col items-center justify-center h-[150vw] md:h-screen gap-4">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center h-[150vw] md:h-screen gap-4">
           <img src={Thinking} height={100} width={100} className="rounded-full"></img>
-          <h1 className="text-[0.75rem] md:text-3xl font-comfortaa">
+          <h1 className="text-center text-[0.75rem] md:text-3xl font-comfortaa">
             Well well, you tried to find a page that doesn't exist
           </h1>
             <Link to="/" className="text-center rounded-md border w-[7rem] bg-[#fffff] hover:cursor-pointer hover:-translate-y-1 duration-200">Return Home</Link>
