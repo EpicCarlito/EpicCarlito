143081d18c35af11f1afcce8cb356a24f33eff4d
EpicCarlito <jameschef2018@hotmail.com>
2023-12-14 18:41:58 -0500

    Changed button to go back

--- src/Pages/Construction.tsx
+++ src/Pages/Construction.tsx
@@ -1,15 +1,17 @@
-import { Link } from "react-router-dom"
 import Construction from "../assets/ConstructionEmoji.svg"
+import { useNavigate } from 'react-router-dom';
 
 export default function Error() {
+  const navigate = useNavigate();
+
     return (
       <div className="h-full">
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-4">
           <img src={Construction} height={100} width={100}></img>
           <h1 className="text-center text-2xl md:text-3xl font-comfortaa md:w-[36rem] w-[24rem]">
-            HEY! These is construction going on, please be safe and go back to the home page!
+            HEY! These is construction going on, please be safe and go back!
           </h1>
-            <Link to="/" className="text-center rounded-lg border w-[12rem] h-[2rem] text-xl bg-[#fffff] hover:cursor-pointer hover:-translate-y-1 duration-200">Return Home</Link>
+          <button onClick={() => navigate(-1)} className="text-center rounded-lg border w-[12rem] h-[2rem] text-xl bg-[#fffff] hover:cursor-pointer hover:-translate-y-1 duration-200">Go Back</button>
         </div>
       </div>
     )
--- src/Pages/Error.tsx
+++ src/Pages/Error.tsx
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom"
 import Thinking from "../assets/ThinkingEmoji.svg"
+import { useNavigate } from 'react-router-dom';
 
 export default function Error() {
+  const navigate = useNavigate();
+
     return (
       <div className="h-full">
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-4">
@@ -9,7 +11,7 @@ export default function Error() {
           <h1 className="text-center text-[0.75rem] md:text-3xl font-comfortaa">
             Well well, you tried to find a page that doesn't exist
           </h1>
-            <Link to="/" className="text-center rounded-md border w-[12rem] h-[2rem] text-xl bg-[#fffff] hover:cursor-pointer hover:-translate-y-1 duration-200">Return Home</Link>
+            <button onClick={() => navigate(-1)} className="text-center rounded-md border w-[12rem] h-[2rem] text-xl bg-[#fffff] hover:cursor-pointer hover:-translate-y-1 duration-200">Go Back</button>
         </div>
       </div>
     )
