56d574fc63915b3876d5f872a35a2c8f6cd5e5b0
EpicCarlito <jameschef2018@hotmail.com>
2023-12-15 09:20:57 -0500

    Navbar adjusted by page

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,10 +1,22 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 export default function NavBar() {
+  const location = useLocation();
     return (
       <div className="absolute flex flex-row justify-center text-xl gap-[1rem] top-4 left-4 md:left-1/2 transform md:-translate-x-1/2">
+        {(location.pathname != "/projects") ? (
         <Link to="/projects" className="rounded-md border w-[7rem] bg-[#fffff] text-center hover:-translate-y-1 duration-200">Projects</Link>
-        <Link to="/hobbies" className="rounded-md border w-[7rem] bg-[#fffff] text-center hover:-translate-y-1 duration-200">Hobbies</Link>  
+        ) : (
+          <Link to="/" className="rounded-md border w-[7rem] bg-[#fffff] text-center hover:-translate-y-1 duration-200">Home</Link>
+        )}
+
+        {(location.pathname != "/hobbies") ? (
+          <Link to="/hobbies" className="rounded-md border w-[7rem] bg-[#fffff] text-center hover:-translate-y-1 duration-200">Hobbies</Link>
+        ): (
+          <Link to="/" className="rounded-md border w-[7rem] bg-[#fffff] text-center hover:-translate-y-1 duration-200">Home</Link>
+        )} 
       </div>
     )
   }
+
+// Check what page the navbar is on so it can decide whether to have the home button or not
