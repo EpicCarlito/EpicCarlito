3ebb17c7c609ce67d8b2f859a591cce812181202
EpicCarlito <jameschef2018@hotmail.com>
2023-12-17 18:15:02 -0500

    Navbar properly adjusts to page

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,18 +3,30 @@ import { Link, useLocation } from "react-router-dom"
 export default function NavBar() {
   const location = useLocation();
     return (
-      <div className="absolute flex flex-row justify-center text-xl gap-[1rem] top-4 left-4 md:left-1/2 transform md:-translate-x-1/2">
-        {(location.pathname != "/projects") ? (
-        <Link to="/projects" className="rounded-md border w-[7rem] bg-[#fffff] text-center hover:-translate-y-1 duration-200">Projects</Link>
-        ) : (
-          <Link to="/" className="rounded-md border w-[7rem] bg-[#fffff] text-center hover:-translate-y-1 duration-200">Home</Link>
-        )}
-
-        {(location.pathname != "/hobbies") ? (
-          <Link to="/hobbies" className="rounded-md border w-[7rem] bg-[#fffff] text-center hover:-translate-y-1 duration-200">Hobbies</Link>
-        ): (
-          <Link to="/" className="rounded-md border w-[7rem] bg-[#fffff] text-center hover:-translate-y-1 duration-200">Home</Link>
-        )} 
-      </div>
+        <div>
+          {(location.pathname != "/") ? (
+            <div className="absolute top-4 left-1/2 transform -translate-x-1/2">
+              <div className="flex flex-row justify-center gap-[1rem] text-xl">
+                {(location.pathname != "/projects") ? (
+                  <Link to="/projects" className="rounded-md border w-[7rem] bg-[#fffff] text-center hover:-translate-y-1 duration-200">Projects</Link>
+                ) : (
+                  <Link to="/" className="rounded-md border w-[7rem] bg-[#fffff] text-center hover:-translate-y-1 duration-200">Go Back</Link>
+                )}
+                {(location.pathname != "/hobbies") ? (
+                  <Link to="/hobbies" className="rounded-md border w-[7rem] bg-[#fffff] text-center hover:-translate-y-1 duration-200">Hobbies</Link>
+                ): (
+                  <Link to="/" className="rounded-md border w-[7rem] bg-[#fffff] text-center hover:-translate-y-1 duration-200">Go Back</Link>
+                )}
+              </div>
+            </div>
+          ) : (
+            <div className="absolute flex flex-row justify-center text-xl gap-[1rem] top-4 left-4 md:left-1/2 transform md:-translate-x-1/2">
+              <Link to="/projects" className="rounded-md border w-[7rem] bg-[#fffff] text-center hover:-translate-y-1 duration-200">Projects</Link>
+              <Link to="/hobbies" className="rounded-md border w-[7rem] bg-[#fffff] text-center hover:-translate-y-1 duration-200">Hobbies</Link>
+            </div>
+          )}
+        </div>
     )
   }
+
+  // Adjust div if not on home page
