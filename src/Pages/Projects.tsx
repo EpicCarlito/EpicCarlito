ea1b1195794a141673b160fa1e34a0cb15bc08fb
EpicCarlito <jameschef2018@gmail.com>
2024-01-29 18:16:05 -0500

    punctuation sir

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -10,20 +10,20 @@ export default function Projects() {
   const projects = [
     {
       name: "Atom Bot",
-      description: "A discord bot that checks email of students to get verified and a message verification system for allowing visitors!",
+      description: "A discord bot that checks email of students to get verified and a message verification system for allowing visitors.",
       type: "typescript",
       date: "June 2023 - Now"
     },
     {
       name: "EmailChecker",
-      description: "A simple way to check if an email exists! Created to be used for the Atom Bot project to check emails",
+      description: "A simple way to check if an email exists! Created to be used for the Atom Bot project to check emails.",
       github: "https://github.com/EpicCarlito/EmailChecker",
       type: "typescript",
       date: "July 2023 - August 2023"
     },
     {
       name: "The Hops Bot",
-      description: "A discord bot that would announce weekly newsletter releases and a nickname system for each user by using a database!",
+      description: "A discord bot that would announce weekly newsletter releases and a nickname system for each user by using a database.",
       type: "typescript",
       date: "October 2022 - May 2023"
     }
