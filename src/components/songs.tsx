14cd810527cbd2354008bb8d91e645657bc35fe2
EpicCarlito <jameschef2018@gmail.com>
2026-01-23 10:34:48 -0500

    Fixed coverflow & name, mobile support going well 🙏

--- src/components/songs.tsx
+++ src/components/songs.tsx
@@ -7,46 +7,51 @@ export default function Members() {
   const [curr, setCurr] = useState<number | null>(null);
 
   return (
-    <div className={styles.membersContainer}>
-      <div className={styles.membersBackground} />
-      <div className={styles.memberContainer}>
-        {songs.map((image, i) => {
-          const middle = Math.floor(songs.length / 2);
-          const offset = curr != null ? i - curr : i - middle;
+    <div className={styles.container}>
+      {songs.map((image, i) => {
+        const middle = Math.floor(songs.length / 2);
+        const offset = curr != null ? i - curr : i - middle;
 
-          const styleClass =
-            curr != null
-              ? offset == 0
-                ? styles.activeImage
-                : offset < 0
-                  ? styles.leftImage
-                  : styles.rightImage
-              : styles.default;
+        const styleClass =
+          curr != null
+            ? offset == 0
+              ? styles.activeImage
+              : offset < 0
+                ? styles.leftImage
+                : styles.rightImage
+            : styles.default;
 
-          return (
-            <div
-              key={i}
-              id={styles.imageContainer}
-              className={styleClass}
-              style={{ "--offset": `${offset}` } as React.CSSProperties}
-              onClick={() => setCurr(i)}
-            >
-              <div className={styles.hoverContainer}>
-                <MoveImage src={image.src} />
-
-                {offset == 0 && curr != null ? (
+        return (
+          <div
+            key={i}
+            className={`${styles.imageContainer} ${styleClass}`}
+            style={{ "--offset": `${offset}` } as React.CSSProperties}
+            onClick={() => setCurr(i)}
+          >
+            {offset == 0 && curr != null ? (
+              <>
+                <MoveImage
+                  // src={
+                  //   "https://stenna.in/cdn/shop/products/34513-1.jpg?v=1663597043"
+                  // }
+                  src={image.src}
+                />
                   <div id={styles.textContainer}>
                     <h1 className={styles.name}>{image.artist}</h1>
                     <h1 className={styles.desc}>{image.desc}</h1>
                   </div>
-                ) : (
-                  <></>
-                )}
-              </div>
-            </div>
-          );
-        })}
-      </div>
+              </>
+            ) : (
+              <img
+                // src={
+                //   "https://images.unsplash.com/photo-1611242320536-f12d3541249b?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cmVkJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D"
+                // }
+                src={image.src}
+              />
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
